50261f1cb8c78afc2fbc0b1edffdfb0049e8247a
caroline BONY <bony.caroline@gmail.com>
2021-12-02 10:54:09 +0100

    render articles

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const newsRouter = express.Router()
+const axios = require('axios')
+
 
 newsRouter.get('', async(req, res) => {
-    res.render('news')
+
+    try {
+        var url = 'https://newsapi.org/v2/top-headlines?' +
+        'country=fr&' +
+        'category=technology&' +
+        'apiKey=47bdc96e56ff4441b525ac9aeffa24dd';
+        const newsAPI = await axios.get(url)
+        res.render('news', { data : newsAPI.data})
+        console.log(newsAPI.data)
+    } catch (error) {
+        if(error.response){
+            console.log(error)
+        }
+
+    }
 })
 
 module.exports = newsRouter
